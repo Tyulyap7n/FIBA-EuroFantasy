0c371f47dcd05a3f94974d94d0c9e90066f7fe5f
Tyulyap7n <dimatyulyapin@yandex.ru>
2025-08-13 13:48:19 +0900

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js
-export const SUPABASE_URL = 'https://xovxokupvsnnjtskdgvr.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhvdnhva3VwdnNubmp0c2tkZ3ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDk4NjMsImV4cCI6MjA3MDQyNTg2M30.Vl5Z9DABFmHQWGtfbyuAZGGgfX4hDYGAPD8C7fr540E';
